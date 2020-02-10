f32ca6ff2ef0a817c53f7b661933455585e262c5
Mohammed Aslam <mdaslamrahman@gmail.com>
2020-02-10 12:43:51 +0530

    initial setup

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function Login() {
+  return (
+    <div className="App">
+      <div>Login</div>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default Login;
